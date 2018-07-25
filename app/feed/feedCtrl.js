8964b7b0e681def4e9e174528b33ef2e9748d1de
iiram <ram8555@gmail.com>
2018-07-25 11:22:12 +0300

    done wiring home and feed pages

--- app/feed/feedCtrl.js
+++ app/feed/feedCtrl.js
@@ -2,6 +2,7 @@ app.controller('feedCtrl', function ($scope, $http, $location, feedSrv) {
     
 
     $scope.testfeedCtrl = "try try feed Ctrl"
+    console.log($scope.testfeedCtrl);
    
   
     });
