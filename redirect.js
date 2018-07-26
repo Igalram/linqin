0be4f6837f9f5d521d33da5687b415ea92516af3
iiram <ram8555@gmail.com>
2018-07-26 15:45:15 +0300

    parsing token

---
+++ redirect.js
@@ -0,0 +1,10 @@
+function handleToken(url) {
+    
+    var token='';
+    token = function getSecondPart(url) {
+        return url.split('=')[1];
+    }
+    console.log(token)
+    
+    return
+}
