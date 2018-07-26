c37a488e197726664048d438082dd07c0a857afe
iiram <ram8555@gmail.com>
2018-07-26 11:03:55 +0300

    attempting redirect uri parser

--- app/app.js
+++ app/app.js
@@ -14,6 +14,25 @@ app.config(function($routeProvider) {
       controller: 'feedCtrl'
     })
 
+    .when('/access_token=:accessToken', {
+      template: '',
+      controller: function ($location,$rootScope) {
+        var hash = $location.path().substr(1);
+        
+        var splitted = hash.split('&');
+        var params = {};
+
+        for (var i = 0; i < splitted.length; i++) {
+          var param  = splitted[i].split('=');
+          var key    = param[0];
+          var value  = param[1];
+          params[key] = value;
+          $rootScope.accesstoken=params;
+        }
+        $location.path("/about");
+      }
+    })
+
 
     .otherwise({
       redirectTo: '/'
