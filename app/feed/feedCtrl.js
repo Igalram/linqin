f56d1a0329ca8048d9d094538cb3366cb5dccc14
saritno <sarit.novak@gmail.com>
2018-07-29 09:53:54 +0300

    for testing

--- app/feed/feedCtrl.js
+++ app/feed/feedCtrl.js
@@ -1,50 +1,50 @@
-angular.module("linquinApp").directive('instagramProfile', function () {
+// angular.module("linquinApp").directive('instagramProfile', function () {
 
-    //https://api.instagram.com/v1/users/self/?access_token=ACCESS-TOKEN
+//     //https://api.instagram.com/v1/users/self/?access_token=ACCESS-TOKEN
 
-    return {
-        restrict: "E",
-        controllerAs: "instagramFeed",
-        templateUrl: "instagramTemplate",
-        transclude: true,
-        controller: function ($scope, $http) {
+//     return {
+//         restrict: "E",
+//         controllerAs: "instagramFeed",
+//         templateUrl: "instagramTemplate",
+//         transclude: true,
+//         controller: function ($scope, $http) {
 
-            var instagramFeed = this;
-            instagramFeed.content = [];
+//             var instagramFeed = this;
+//             instagramFeed.content = [];
 
-            $http({
-                url: 'https://api.instagram.com/v1/users/self/media/recent',
-                params: {
-                    access_token: instagram_key
-                }
-            }).then(function (response) {
-                console.log(response);
+//             $http({
+//                 url: 'https://api.instagram.com/v1/users/self/media/recent',
+//                 params: {
+//                     access_token: instagram_key
+//                 }
+//             }).then(function (response) {
+//                 console.log(response);
 
-                //userInfo
-                var userInfo = response.data.data[0].user;
-                instagramFeed.full_name = userInfo.full_name;
-                instagramFeed.username = userInfo.username;
-                instagramFeed.profile_picture = userInfo.profile_picture;
+//                 //userInfo
+//                 var userInfo = response.data.data[0].user;
+//                 instagramFeed.full_name = userInfo.full_name;
+//                 instagramFeed.username = userInfo.username;
+//                 instagramFeed.profile_picture = userInfo.profile_picture;
 
-                //user Images
-                angular.forEach(response.data.data, function (Object, key) {
-                    instagramFeed.content[key] = {};
-                    instagramFeed.content[key].imageUrl = object.images.standard_resolution.url;
-                    instagramFeed.content[key].instagramLink = object.link;
-                })
-            })
-        }
-    }
-})
-
-
-// app.controller('feedCtrl', function ($scope, $http, $location, feedSrv) {
+//                 //user Images
+//                 angular.forEach(response.data.data, function (Object, key) {
+//                     instagramFeed.content[key] = {};
+//                     instagramFeed.content[key].imageUrl = object.images.standard_resolution.url;
+//                     instagramFeed.content[key].instagramLink = object.link;
+//                 })
+//             })
+//         }
+//     }
+// })
 
 
-//     $scope.testfeedCtrl = "try try feed Ctrl"
-//     console.log($scope.testfeedCtrl);
+app.controller('feedCtrl', function ($scope, $http, $location, feedSrv) {
 
 
-//     });
+    $scope.testfeedCtrl = "try try feed Ctrl"
+    console.log($scope.testfeedCtrl);
+
+
+    });
 
 
