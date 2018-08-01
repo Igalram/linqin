d36c5a9766fda7e6f8825d2355f7b1237565139d
iiram <ram8555@gmail.com>
2018-08-01 12:37:37 +0300

    putting function in response

--- app/feed/feedCtrl.js
+++ app/feed/feedCtrl.js
@@ -28,14 +28,21 @@ app.controller('feedCtrl', function ($scope, $q, $http, $location, feedSrv) {
     $http.get(request).then(function (response) {
         //userInfo
         var userInfo = response.data.data[0].user;
-        console.log (userInfo);
+        console.log(userInfo);
         $scope.userName = userInfo.username;
         $scope.userId = userInfo.id;
+        $scope.userExists = false;
+
         $scope.profilePicture = userInfo.profile_picture;
         $scope.fullName = userInfo.fullName;
 
+        function checkUserExists($scope.userId) {
+            console.log("userID from function: " + $scope.userId);
+
+        }
+
         console.log("username" + userInfo.username);
-        
+
         $scope.content = response.data;
         console.log($scope.content);
 
@@ -45,11 +52,7 @@ app.controller('feedCtrl', function ($scope, $q, $http, $location, feedSrv) {
 
     console.log("user Info:" + $scope.userInfo);
 
-    $scope.userExists = false;
-    function checkUserExists($scope.userId) {
-        console.log("userID from function: " + $scope.userId);
 
-    }
 
     function Post(id, userId, img, date, likes, link, location) {
         this.id = id;
@@ -83,65 +86,65 @@ app.controller('feedCtrl', function ($scope, $q, $http, $location, feedSrv) {
 
 
 
-    //console.log("$scope.content: " + $scope.content);
+//console.log("$scope.content: " + $scope.content);
 
 
 
 
-    // $scope.getFeed = function (token) {
+// $scope.getFeed = function (token) {
 
-    //     $scope.token = localStorage.getItem("token");
-    //     window.alert($scope.token);
-    //     var request = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + $scope.token;
-    //     var async = $q.defer();
-    //     $http.get(request).then(function (response) {
-    //         //$scope.content = response.data;
+//     $scope.token = localStorage.getItem("token");
+//     window.alert($scope.token);
+//     var request = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + $scope.token;
+//     var async = $q.defer();
+//     $http.get(request).then(function (response) {
+//         //$scope.content = response.data;
 
-    //         //userInfo
-    //         var userInfo = response.data.data[0].user;
-    //         console.log("username" + userInfo.full_name);
+//         //userInfo
+//         var userInfo = response.data.data[0].user;
+//         console.log("username" + userInfo.full_name);
 
 
-    //         async.resolve(request);
-    //     }, function (error) {
-    //         console.error(error);
-    //         async.reject("Failed to get feed");
-    //     })
+//         async.resolve(request);
+//     }, function (error) {
+//         console.error(error);
+//         async.reject("Failed to get feed");
+//     })
 
-    //     console.log("user Info:" + $scope.userInfo);
-    //     //console.log("$scope.content: " + $scope.content);
-    //     return async.promise;
-    // }
+//     console.log("user Info:" + $scope.userInfo);
+//     //console.log("$scope.content: " + $scope.content);
+//     return async.promise;
+// }
 
-    // return {
-    //     restrict: "E",
-    //     //controllerAs: "instagramFeed",
-    //     //templateUrl: "instagramTemplate",
-    //     transclude: true,
-    //     controller: function ($scope, $http) {
+// return {
+//     restrict: "E",
+//     //controllerAs: "instagramFeed",
+//     //templateUrl: "instagramTemplate",
+//     transclude: true,
+//     controller: function ($scope, $http) {
 
-    //         var instagramFeed = this;
-    //         instagramFeed.content = [];
+//         var instagramFeed = this;
+//         instagramFeed.content = [];
 
-    //         $http({
-    //             url: 'https://api.instagram.com/v1/users/self/media/recent',
-    //             params: {
-    //                 access_token: instagram_key
-    //             }
-    //         }).then(function (response) {
-    //             console.log(response);
+//         $http({
+//             url: 'https://api.instagram.com/v1/users/self/media/recent',
+//             params: {
+//                 access_token: instagram_key
+//             }
+//         }).then(function (response) {
+//             console.log(response);
 
-    //             //userInfo
-    //             var userInfo = response.data.data[0].user;
-    //             instagramFeed.full_name = userInfo.full_name;
-    //             instagramFeed.username = userInfo.username;
-    //             instagramFeed.profile_picture = userInfo.profile_picture;
+//             //userInfo
+//             var userInfo = response.data.data[0].user;
+//             instagramFeed.full_name = userInfo.full_name;
+//             instagramFeed.username = userInfo.username;
+//             instagramFeed.profile_picture = userInfo.profile_picture;
 
-    //             //user Images
-    //             angular.forEach(response.data.data, function (Object, key) {
-    //                 instagramFeed.content[key] = {};
-    //                 instagramFeed.content[key].imageUrl = object.images.standard_resolution.url;
-    //                 instagramFeed.content[key].instagramLink = object.link;
-    //             })
-    //         })
-    //     }
+//             //user Images
+//             angular.forEach(response.data.data, function (Object, key) {
+//                 instagramFeed.content[key] = {};
+//                 instagramFeed.content[key].imageUrl = object.images.standard_resolution.url;
+//                 instagramFeed.content[key].instagramLink = object.link;
+//             })
+//         })
+//     }
