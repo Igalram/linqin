b370057520992e969380e0bc70d46375575472b3
saritno <sarit.novak@gmail.com>
2018-08-02 15:56:39 +0300

    fixes

--- app/feed/feedCtrl.js
+++ app/feed/feedCtrl.js
@@ -41,9 +41,12 @@ app.controller('feedCtrl', function ($scope, $q, $http, $location, $routeParams,
         for (i = 0; i < DB.data.users.length; i++) {
             console.log("user=" + DB.data.users[i]);
             console.log("userId=" + DB.data.users[i].data[0].user.id);
-            if (userId === DB.data.users[i].id)
-                return true;
+            if (userId === DB.data.users[i].id){
+                console.log("true");
+                return true;}
+            
         }
+        console.log("false");
         return false;
     }
 
@@ -116,70 +119,3 @@ app.controller('feedCtrl', function ($scope, $q, $http, $location, $routeParams,
 
 
 
-
-
-
-
-
-//console.log("$scope.content: " + $scope.content);
-
-
-
-
-// $scope.getFeed = function (token) {
-
-//     $scope.token = localStorage.getItem("token");
-//     window.alert($scope.token);
-//     var request = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + $scope.token;
-//     var async = $q.defer();
-//     $http.get(request).then(function (response) {
-//         //$scope.content = response.data;
-
-//         //userInfo
-//         var userInfo = response.data.data[0].user;
-//         console.log("username" + userInfo.full_name);
-
-
-//         async.resolve(request);
-//     }, function (error) {
-//         console.error(error);
-//         async.reject("Failed to get feed");
-//     })
-
-//     console.log("user Info:" + $scope.userInfo);
-//     //console.log("$scope.content: " + $scope.content);
-//     return async.promise;
-// }
-
-// return {
-//     restrict: "E",
-//     //controllerAs: "instagramFeed",
-//     //templateUrl: "instagramTemplate",
-//     transclude: true,
-//     controller: function ($scope, $http) {
-
-//         var instagramFeed = this;
-//         instagramFeed.content = [];
-
-//         $http({
-//             url: 'https://api.instagram.com/v1/users/self/media/recent',
-//             params: {
-//                 access_token: instagram_key
-//             }
-//         }).then(function (response) {
-//             console.log(response);
-
-//             //userInfo
-//             var userInfo = response.data.data[0].user;
-//             instagramFeed.full_name = userInfo.full_name;
-//             instagramFeed.username = userInfo.username;
-//             instagramFeed.profile_picture = userInfo.profile_picture;
-
-//             //user Images
-//             angular.forEach(response.data.data, function (Object, key) {
-//                 instagramFeed.content[key] = {};
-//                 instagramFeed.content[key].imageUrl = object.images.standard_resolution.url;
-//                 instagramFeed.content[key].instagramLink = object.link;
-//             })
-//         })
-//     }
