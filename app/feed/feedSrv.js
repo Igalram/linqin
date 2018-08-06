7203e24daa112272046c0dacae0dbbfc2684287a
Igalram <ram8555@gmail.com>
2018-08-06 11:04:43 +0300

    drafting update functionality

--- app/feed/feedSrv.js
+++ app/feed/feedSrv.js
@@ -6,8 +6,41 @@ app.factory('feedSrv', function ($http, $log, $q) {
     console.log(testSrv);
     console.log("token from feedSrv=" + token);
 
+    function getOffset(IgObject, ourDB, userIndex) {
+        for (i=IgObject.data.data.length; i=0; i--) {
+
+            for (j=ourDB.data.users[userIndex].data.length; j=0; j--) {
+
+                if (IgObject.data.data.id==ourDB.data.users[userIndex].data.id) {
+                    offSet=(j-i);
+                    return offSet;
+
+                }
+
+            }
+
+
+
+        }
+
+
+    }
+
+
+    function updateDB (IgObject, ourDB, offSet) {
+        
+        newIgObject = IgObject.slice(0,offSet);
+        newDB = newIgObject.concat(ourDB);
+        http.patch (path, newDB);
+
+
+    }
+
+
     return {
-        token: token
+        token: token,
+        updateDB : updateDB,
+        getOffset : getOffset
     }
 });
 
