1960ff4871d17ba779ae4e22abe8653a48f882d1
saritno <sarit.novak@gmail.com>
2018-08-13 14:06:42 +0300

    sdasda

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -2,15 +2,15 @@ app.controller("homeCtrl", function ($scope, $q, $http, $routeParams, $location,
   $scope.testhomeCtrl = "try try home Ctrl";
   $scope.receivedToken = 'asdf';
   var token = $location.$$url.substring(15);
-  
+
   //new code for dynamic redirect
   var host = $location.host();
   $scope.host = host;
-  if (host=="localhost"){host+=":5500";}
-  else if (host=="127.0.0.1"){host+=":5500";}
+  if (host == "localhost") { host += ":5500"; }
+  else if (host == "127.0.0.1") { host += ":5500"; }
   console.log(host);
-  
-  
+
+
   if (token) {
     feedSrv.token = token;
     localStorage.setItem("token", token);
