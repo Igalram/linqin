e84c962f331bf3114d33bb7ede5c0420794dd1dc
Igalram <ram8555@gmail.com>
2018-08-13 13:34:00 +0300

    dynamic redirect

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -2,6 +2,14 @@ app.controller("homeCtrl", function ($scope, $q, $http, $routeParams, $location,
   $scope.testhomeCtrl = "try try home Ctrl";
   $scope.receivedToken = 'asdf';
   var token = $location.$$url.substring(15);
+  
+  //new code for dynamic redirect
+  var host = $location.host();
+  if (host=="localhost"){host+=":5500";}
+  else if (host=="127.0.0.1"){host+=":5500";}
+  console.log(host);
+  debugger
+  
   if (token) {
     feedSrv.token = token;
     localStorage.setItem("token", token);
